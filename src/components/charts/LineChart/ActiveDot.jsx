e59be5b1c0d168d79366e6a1a646b9f17fb11fb6
Tal Kohavy <talkohavy@gmail.com>
2024-04-13 11:50:50 +0300

    activeDot fill color is taken from line now

--- src/components/charts/LineChart/ActiveDot.jsx
+++ src/components/charts/LineChart/ActiveDot.jsx
@@ -1,11 +1,11 @@
 export default function ActiveDot(props) {
-  const { cx, cy, data, index } = props;
+  const { cx, cy, data, index, fill } = props;
 
   const dot = data[index]?.dot;
   const dotProps = {
     r: dot?.r ?? 8,
     stroke: dot?.stroke ?? 'white',
-    fill: '#ff7300',
+    fill: dot?.fill ?? fill,
   };
 
   return (
