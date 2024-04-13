b6f82a36bcbd7a7a3fe7e53490a188e6a1006b43
Tal Kohavy <talkohavy@gmail.com>
2024-04-13 12:29:45 +0300

    fixed CustomAxisTick - better positioning of X

--- src/components/charts/CustomAxisTick.jsx
+++ src/components/charts/CustomAxisTick.jsx
@@ -1,9 +1,11 @@
-import { formatLabel } from './helpers';
+import { formatLabel, getTextWidth } from './helpers';
 
 /** @param {import('recharts').LabelProps & {payload: any}} props */
 export function CustomizedAxisTick(props) {
   const { x, y, color, payload, angle, textAnchor, fontWeight, fontSize } = props;
-  const translateXBy = +x + (angle < -45 ? -10 : angle ? 0 : 30);
+  const formattedLabel = formatLabel(payload.value);
+  const textWidth = getTextWidth({ text: formattedLabel });
+  const translateXBy = +x + (angle < -45 ? -10 : angle ? 0 : textWidth / 2);
 
   return (
     <g transform={`translate(${translateXBy},${y})`}>
@@ -18,7 +20,7 @@ export function CustomizedAxisTick(props) {
         transform={angle ? `rotate(${angle})` : undefined}
         style={{ fontSize, fontWeight }}
       >
-        {formatLabel(payload.value)}
+        {formattedLabel}
       </text>
     </g>
   );
