fdcd9674db81044afe6278a211607c798b24ae5d
Tal Kohavy <talkohavy@gmail.com>
2024-08-05 13:13:57 +0300

    ActiveDot small change

--- src/components/charts/LineChart/ActiveDot.tsx
+++ src/components/charts/LineChart/ActiveDot.tsx
@@ -4,24 +4,22 @@ type ActiveDotProps = DotProps & {
   payload: any;
   dataKey: string;
   data: Array<any>;
+  showChartValues: boolean;
+  showLineValues: boolean;
 };
 
 export default function ActiveDot(props: ActiveDotProps) {
-  const { cx, cy, payload, dataKey, fill, r, data } = props;
+  const { cx, cy, payload, dataKey, data, r, fill } = props;
 
   if (!payload[dataKey]) return;
 
   const { dot } = data.find((dotData) => dotData.x === payload.x && dotData.y === payload[dataKey]) ?? {};
 
   const dotProps = { r: (dot?.r ?? r) * 1.1 + 2, fill: dot?.fill ?? fill, stroke: dot?.stroke ?? 'white' };
-  // const isValueVisible = showDotValue ?? showDotValues;
 
   return (
     <svg>
       <circle cx={cx} cy={cy} {...dotProps} />
-      {/* <text x={cx} y={cy} dy={4} textAnchor='middle' fontSize={9}>
-        {isValueVisible && formatLabel(payload[dataKey])}
-      </text> */}
     </svg>
   );
 }
